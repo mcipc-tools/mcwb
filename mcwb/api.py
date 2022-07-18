df6a796192e92eace9b2e53f7bc287cdb0841adc
Giles Knap <gilesknap@gmail.com>
2022-07-18 21:12:02 +0100

    poly function working well

--- mcwb/api.py
+++ mcwb/api.py
@@ -1,7 +1,8 @@
 """Exposed API functions."""
 
 import math
-from typing import List
+from time import sleep
+from typing import List, Optional
 
 from mcipc.rcon.client import Client
 from mcipc.rcon.enumerations import FillMode, Item
@@ -9,35 +10,42 @@ from mcipc.rcon.enumerations import FillMode, Item
 from mcwb.functions import get_direction, normalize, offsets, validate
 from mcwb.types import Anchor, Profile, Vec3
 
-__all__ = ['mktunnel']
+__all__ = ["mktunnel"]
 
 
-def mktunnel(client: Client, profile: Profile, start: Vec3, *,
-             end: Vec3 = None, direction: Vec3 = None, length: int = 1,
-             anchor: Anchor = Anchor.BOTTOM_RIGHT, default: Item = Item.AIR,
-             mode: FillMode = None, filter: str = None):
+def mktunnel(
+    client: Client,
+    profile: Profile,
+    start: Vec3,
+    *,
+    end: Vec3 = None,
+    direction: Vec3 = None,
+    length: int = 1,
+    anchor: Anchor = Anchor.BOTTOM_RIGHT,
+    default: Item = Item.AIR,
+    mode: FillMode = None,
+    filter: str = None,
+):
     """Creates a tunnel with the given profile."""
 
-    start = Vec3(*start)    # Ensure Vec3 object.
+    start = Vec3(*start)  # Ensure Vec3 object.
 
     if validate(profile) != 2:
-        raise ValueError('Invalid matrix.')
+        raise ValueError("Invalid matrix.")
 
     if end is None:
         end = start + direction * length
     else:
-        end = Vec3(*end)    # Ensure Vec3 object.
+        end = Vec3(*end)  # Ensure Vec3 object.
 
     calculated_direction = get_direction(start, end)
     profile = list(normalize(profile, default=default))
 
     for block, offset in offsets(profile, calculated_direction, anchor):
-        client.fill(
-            start + offset, end + offset, block, mode=mode, filter=filter
-        )
+        client.fill(start + offset, end + offset, block, mode=mode, filter=filter)
 
 
-def polygon(diameter: int, sides: int):
+def polygon(diameter: int, sides: int, offset: Optional[float] = None):
     """
     Creates a polygon with the given diameter and number of sides.
     """
@@ -45,28 +53,32 @@ def polygon(diameter: int, sides: int):
     corners: List[Vec3] = []
 
     if sides < 3:
-        raise ValueError('Number of sides must be at least 3.')
+        raise ValueError("Number of sides must be at least 3.")
 
     radius = diameter // 2
     angle = 2 * math.pi / sides
-    offset = angle / 2
+    offset = angle / 2 if offset is None else offset
     for i in range(sides):
-        x = radius * math.cos(i * angle + offset)
-        z = radius * math.sin(i * angle + offset)
+        x = round(radius * math.cos(i * angle + offset))
+        z = round(radius * math.sin(i * angle + offset))
         corners.append(Vec3(x, 0, z))
 
     return corners
 
 
-def square(client: Client, center: Vec3, item: Item = Item.AIR):
-    corners = polygon(diameter=12, sides=4)
+def poly(
+    client: Client, center: Vec3, sides=4, dia=15, item: Item = Item.AIR, offset=None
+):
+    corners = polygon(diameter=dia, sides=sides, offset=offset)
     for i in range(len(corners)):
-        make_line_xz(client, center + corners[i], center + corners[(i+1) % 4], item)
+        make_line_xz(
+            client, center + corners[i], center + corners[(i + 1) % sides], item
+        )
+        sleep(1)
 
 
 def make_line_xz(
-    client: Client, start: Vec3, end: Vec3, item: Item = Item.AIR,
-    overlap: int = 0
+    client: Client, start: Vec3, end: Vec3, item: Item = Item.AIR, overlap: int = 0
 ):
     """
     Creates a line of blocks from start to end.
@@ -74,18 +86,31 @@ def make_line_xz(
     The line is at the height of the start block and has constant height.
     """
 
-    start = Vec3(*start).with_ints()    # Ensure Vec3 object.
-    end = Vec3(*end).with_ints()        # Ensure Vec3 object.
-    x_step = 1 if start.x <= end.x else -1
+    def d_range(n1, n2):
+        if n1 > n2:
+            return -1, range(n1, n2 - 1, -1), n1 - n2
+        else:
+            return 1, range(n1, n2 + 1), n2 - n1
 
-    dx = end.x - start.x
-    dz = end.z - start.z
-    z_step = dz if dx == 0 else dz / abs(start.x - end.x)
-    y, z = start.y, start.z
+    start = Vec3(*start).with_ints()  # Ensure Vec3 object.
+    end = Vec3(*end).with_ints()  # Ensure Vec3 object.
 
-    for x in range(int(start.x), int(end.x + 1), x_step):
-        next_z = z + z_step
-        min_z, max_z = min(next_z, z), max(next_z, z)
-        for tmp_z in range(round(min_z), round(max_z+1)):
-            client.setblock(Vec3(x, y, tmp_z), item)
-        z = next_z
+    dir_x, range_x, delta_x = d_range(start.x, end.x)
+    dir_z, range_z, delta_z = d_range(start.z, end.z)
+
+    steps = int(max(delta_x, delta_z))
+
+    z_step = delta_z / steps * dir_z
+    x_step = delta_x / steps * dir_x
+
+    x, y, z = start.x, start.y, start.z
+    next_x, next_z = x, z
+
+    client.setblock(Vec3(x, y, z), item)
+
+    for i in range(steps + 1):
+        if (x, z) != (round(next_x), round(next_z)):
+            x, z = round(next_x), round(next_z)
+            client.setblock(Vec3(x, y, z), item)
+        next_z += z_step
+        next_x += x_step
